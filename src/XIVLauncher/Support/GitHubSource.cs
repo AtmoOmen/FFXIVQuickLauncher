809f8d22ac8a263d6a663897d75da4b70f9eede5
status102 <102887808+status102@users.noreply.github.com>
2025-03-15 10:01:01 +0800

    perf: 移除不再使用的重载

--- src/XIVLauncher/Support/GitHubSource.cs
+++ src/XIVLauncher/Support/GitHubSource.cs
@@ -23,21 +23,6 @@ public class GitHubSource : Velopack.Sources.GithubSource
     {
     }
 
-    protected override async Task<GithubRelease[]> GetReleases(bool includePrereleases)
-    {
-        // https://docs.github.com/en/rest/reference/releases
-        const int perPage = 10;
-        const int page = 1;
-        var releasesPath = $"repos{RepoUri.AbsolutePath}/releases?per_page={perPage}&page={page}";
-        var baseUri = GetApiBaseUrl(RepoUri);
-        var getReleasesUri = new Uri(baseUri, releasesPath);
-        var response = await Downloader.DownloadString(getReleasesUri.ToString(), Authorization, "application/vnd.github.v3+json").ConfigureAwait(false);
-        var releases = JsonConvert.DeserializeObject<List<GithubRelease>>(response);
-        if (releases == null)
-            return [];
-        return releases.OrderByDescending(d => d.PublishedAt).Where(x => includePrereleases || !x.Prerelease).ToArray();
-    }
-
     public override async Task<VelopackAssetFeed> GetReleaseFeed(ILogger logger, string channel, Guid? stagingId = null, VelopackAsset? latestLocalRelease = null)
     {
         var releases = await GetReleases(Prerelease).ConfigureAwait(false);
