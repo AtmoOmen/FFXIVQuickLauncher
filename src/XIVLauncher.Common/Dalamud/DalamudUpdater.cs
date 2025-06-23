cf79d70cb5bb3f6f3bffb6458858408b0a1165b7
AtmoOmen <atmoomen2004@outlook.com>
2025-06-23 15:36:58 +0800

    1.2.3

--- src/XIVLauncher.Common/Dalamud/DalamudUpdater.cs
+++ src/XIVLauncher.Common/Dalamud/DalamudUpdater.cs
@@ -40,7 +40,7 @@ public class DalamudUpdater
     public static string                  OnlineHash          { get; private set; } = string.Empty;
     public static string                  Version             { get; private set; } = string.Empty;
 
-    public const string RuntimeVersion = "8.0.3";
+    public const string RuntimeVersion = "9.0.2";
 
     public FileInfo Runner
     {
