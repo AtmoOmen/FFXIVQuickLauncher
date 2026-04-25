4a43ebbb7e7874d2f82d1aeab280be953249b2e4
AtmoOmen <atmoomen2004@outlook.com>
2026-04-25 15:32:54 +0800

    LoginArea

--- src/XIVLauncher.Common/Game/Login/LoginArea.cs
+++ src/XIVLauncher.Common/Game/Login/LoginArea.cs
@@ -4,38 +4,51 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using XIVLauncher.Common.Constant;
+using XIVLauncher.Common.Http;
 
 namespace XIVLauncher.Common.Game.Login;
 
 public class LoginArea
 {
-    [JsonProperty("Areaid")]           public string AreaID           { get; set; } = null!;
-    [JsonProperty("AreaStat")]         public int    AreaStatus       { get; set; }
-    [JsonProperty("AreaOrder")]        public int    AreaOrder        { get; set; }
-    [JsonProperty("AreaName")]         public string AreaName         { get; set; } = null!;
-    [JsonProperty("Areatype")]         public int    AreaType         { get; set; }
-    [JsonProperty("AreaLobby")]        public string AreaLobby        { get; set; } = null!;
-    [JsonProperty("AreaGm")]           public string AreaGM           { get; set; } = null!;
-    [JsonProperty("AreaPatch")]        public string AreaPatch        { get; set; } = null!;
-    [JsonProperty("AreaConfigUpload")] public string AreaConfigUpload { get; set; } = null!;
+    private static readonly HttpClient Client = XLHttpClientFactory.Create(TimeSpan.FromSeconds(30), int.MaxValue, DecompressionMethods.None);
+
+    static LoginArea() =>
+        Client.Timeout = TimeSpan.FromSeconds(30);
+
+    [JsonProperty("Areaid")]
+    public string AreaID { get; set; } = null!;
+
+    [JsonProperty("AreaStat")]
+    public int AreaStatus { get; set; }
+
+    [JsonProperty("AreaOrder")]
+    public int AreaOrder { get; set; }
+
+    [JsonProperty("AreaName")]
+    public string AreaName { get; set; } = null!;
+
+    [JsonProperty("Areatype")]
+    public int AreaType { get; set; }
+
+    [JsonProperty("AreaLobby")]
+    public string AreaLobby { get; set; } = null!;
+
+    [JsonProperty("AreaGm")]
+    public string AreaGM { get; set; } = null!;
+
+    [JsonProperty("AreaPatch")]
+    public string AreaPatch { get; set; } = null!;
+
+    [JsonProperty("AreaConfigUpload")]
+    public string AreaConfigUpload { get; set; } = null!;
 
     public static async Task<LoginArea[]> Get()
     {
-        var handler = new HttpClientHandler
-        {
-            UseProxy    = true,
-            Proxy       = WebRequest.GetSystemWebProxy(),
-            Credentials = CredentialCache.DefaultCredentials
-        };
-
-        using var client = new HttpClient(handler);
-        client.Timeout = TimeSpan.FromSeconds(30);
-
         var request = new HttpRequestMessage(HttpMethod.Get, Links.SDO_LOGIN_AREA_URL);
         request.Headers.Add("Accept", "*/*");
         request.Headers.Add("Host",   "ff.dorado.sdo.com");
 
-        using var response = await client.SendAsync(request);
+        using var response = await Client.SendAsync(request);
         response.EnsureSuccessStatusCode();
 
         var text = await response.Content.ReadAsStringAsync();
