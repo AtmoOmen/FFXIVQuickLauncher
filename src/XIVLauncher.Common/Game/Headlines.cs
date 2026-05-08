b833766e6e54b89b0d34fdf3bef39783a3c7c925
FatChocobo <maplerecall@live.com>
2026-05-09 01:33:50 +0800

    dedup news

--- src/XIVLauncher.Common/Game/Headlines.cs
+++ src/XIVLauncher.Common/Game/Headlines.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
@@ -44,6 +45,18 @@ namespace XIVLauncher.Common.Game
 
         [JsonProperty("fix_order")]
         public int? FixOrder { get; set; }
+
+        public int? NewsId
+        {
+            get
+            {
+                if (this.Link == null)
+                    return null;
+
+                var queryParams = HttpUtility.ParseQueryString(this.Link.Query);
+                return int.TryParse(queryParams["id"], NumberStyles.Integer, CultureInfo.InvariantCulture, out var newsId) ? newsId : null;
+            }
+        }
     }
 
     public class BannerRoot
@@ -93,14 +106,26 @@ namespace XIVLauncher.Common.Game
         {
             var headlines = new Headlines();
             headlines.Banner = await GetBanner(game);
-            var bannerTitles = new HashSet<string>((headlines.Banner ?? Array.Empty<Banner>())
+
+            var banners = headlines.Banner ?? Array.Empty<Banner>();
+            var bannerTitles = new HashSet<string>(banners
                 .Where(banner => !string.IsNullOrWhiteSpace(banner.Title))
                 .Select(banner => banner.Title), StringComparer.Ordinal);
+            var bannerNewsIds = new HashSet<int>(banners
+                .Select(banner => banner.NewsId)
+                .Where(newsId => newsId.HasValue)
+                .Select(newsId => newsId.Value));
 
             headlines.News = (await GetNews(game))?
-                .Where(news => !bannerTitles.Contains(news.Title))
+                .Where(news => !IsBannerNews(news, bannerTitles, bannerNewsIds))
                 .ToArray();
             return headlines;
+        }
+
+        private static bool IsBannerNews(News news, HashSet<string> bannerTitles, HashSet<int> bannerNewsIds)
+        {
+            return bannerTitles.Contains(news.Title) ||
+                   int.TryParse(news.Id, NumberStyles.Integer, CultureInfo.InvariantCulture, out var newsId) && bannerNewsIds.Contains(newsId);
         }
 
         private static async Task<Banner[]> GetBanner(Launcher game) {
@@ -111,7 +136,7 @@ namespace XIVLauncher.Common.Game
 
         private static async Task<News[]> GetNews(Launcher game)
         {
-            var json = Encoding.UTF8.GetString(await game.DownloadAsLauncher("https://cqnews.web.sdo.com/api/news/newsList?gameCode=ff&CategoryCode=8324,8325,8326,8327,5309,5310,5311,5312,5313&pageIndex=0&pageSize=12", ClientLanguage.ChineseSimplified, "*/*").ConfigureAwait(false));
+            var json = Encoding.UTF8.GetString(await game.DownloadAsLauncher("https://cqnews.web.sdo.com/api/news/newsList?gameCode=ff&CategoryCode=8324,8325,8326,8327,5309,5310,5311,5312,5313&pageIndex=0&pageSize=16", ClientLanguage.ChineseSimplified, "*/*").ConfigureAwait(false));
             var sdoNews = JsonConvert.DeserializeObject<SdoNews>(json);
             return sdoNews.Data;
         }
