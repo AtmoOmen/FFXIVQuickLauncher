266b8d995bdbb4128b0037fa0da62e95aaaf5e5e
AtmoOmen <atmoomen2004@outlook.com>
2026-08-28 00:48:46 +0800

    重构

--- src/XIVLauncher.Common/Game/Launcher.cs
+++ src/XIVLauncher.Common/Game/Launcher.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using Serilog;
 using XIVLauncher.Common.Constant;
@@ -8,12 +9,14 @@ namespace XIVLauncher.Common.Game;
 
 public partial class Launcher
 {
+    public delegate Process? GameStarter(GameStartRequest request);
+
     public RestartMonitor RestartMonitor { get; } = new();
     public HttpClient     MockHttpClient { get; } = new(new HttpClientHandler { UseCookies = true });
 
     public FFXIVProcess? LaunchGame
     (
-        IGameRunner   runner,
+        GameStarter   startGame,
         string        sessionID,
         string        sndaID,
         int           dcTravelPort,
@@ -61,7 +64,7 @@ public partial class Launcher
                             ? argumentBuilder.BuildEncrypted()
                             : argumentBuilder.Build();
 
-        var process = runner.Start(exePath, workingDir, arguments, environment, dpiAwareness);
+        var process = startGame(new GameStartRequest(exePath, workingDir, arguments, environment, dpiAwareness));
         return process != null ? new FFXIVProcess(process) : null;
     }
 
