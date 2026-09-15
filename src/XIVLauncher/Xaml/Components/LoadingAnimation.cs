ce658732e567105b5effcd9fde58541e793a790f
AtmoOmen <atmoomen2004@outlook.com>
2026-09-15 17:46:23 +0800

    2.4.8

--- src/XIVLauncher/Xaml/Components/LoadingAnimation.cs
+++ src/XIVLauncher/Xaml/Components/LoadingAnimation.cs
@@ -1,72 +1,209 @@
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
 
 namespace XIVLauncher.Xaml.Components;
 
 public sealed class LoadingAnimation : FrameworkElement
 {
-    private const int    FRAME_COUNT   = 720;
-    private const int    ATLAS_COLUMNS = 20;
-    private const double LOOP_SECONDS  = 3;
-    
-    private const int ATLAS_DECODE_WIDTH = 1600;
+    private const double DESIGN_SIZE       = 160;
+    private const double RING_RADIUS       = 50;
+    private const double RING_THICKNESS    = 6;
+    private const double RIPPLE_RADIUS     = 47;
+    private const double RIPPLE_MAX_RADIUS = 80;
 
-    private const int PLAYBACK_FRAME_RATE = 60;
+    private const double ARC_START_ANGLE = 356.7;
+    private const double ARC_SWEEP_ANGLE = 256.9;
 
-    private static readonly BitmapSource Atlas = LoadAtlas();
+    private const double LOOP_SECONDS = 3;
+    private const double LOOP_DEGREES = 720;
 
-    private static readonly DependencyProperty FrameIndexProperty = DependencyProperty.Register
-    (
-        "FrameIndex",
-        typeof(int),
-        typeof(LoadingAnimation),
-        new PropertyMetadata(0, OnFrameIndexChanged)
-    );
+    private const double TRACK_OPACITY      = 38d / 255d;
+    private const double RIPPLE_OPACITY     = 64d / 255d;
+    private const double RIPPLE_MIN_OPACITY = 13d / 255d;
 
-    private readonly ImageBrush frameBrush = new(Atlas)
-    {
-        Viewbox      = new Rect(0, 0, 1d / ATLAS_COLUMNS, (double)ATLAS_COLUMNS / FRAME_COUNT),
-        ViewboxUnits = BrushMappingMode.RelativeToBoundingBox,
-        Stretch      = Stretch.Fill
-    };
+    private static readonly Point CENTER = new(DESIGN_SIZE / 2, DESIGN_SIZE / 2);
+    private static readonly Color TINT   = Color.FromRgb(0xFF, 0x97, 0x00);
 
-    private Window?         hostWindow;
-    private bool            isPlaying;
-    private AnimationClock? playbackClock;
-    private TimeSpan        position;
+    private static readonly Geometry RIPPLE_GEOMETRY = Freeze(new EllipseGeometry(CENTER, RIPPLE_RADIUS, RIPPLE_RADIUS));
+    private static readonly Geometry TRACK_GEOMETRY  = Freeze(new EllipseGeometry(CENTER, RING_RADIUS,   RING_RADIUS));
+    private static readonly Geometry ARC_GEOMETRY    = Freeze(CreateArcGeometry());
+
+    private static readonly Pen TRACK_PEN = CreatePen(TRACK_OPACITY);
+    private static readonly Pen ARC_PEN   = CreatePen(1);
+
+    private readonly DrawingVisual   contentVisual = new();
+    private readonly ScaleTransform  sizeTransform = new();
+    private readonly ScaleTransform  rippleScale   = new(1, 1, CENTER.X, CENTER.Y);
+    private readonly RotateTransform arcRotation   = new(0, CENTER.X, CENTER.Y);
+    private readonly SolidColorBrush rippleBrush   = new(TINT) { Opacity = RIPPLE_OPACITY };
+
+    private List<AnimationClock>? clocks;
+    private Window?               hostWindow;
+    private bool                  isPlaying;
 
     public LoadingAnimation()
     {
-        IsHitTestVisible =  false;
+        IsHitTestVisible = false;
+
+        contentVisual.Transform = sizeTransform;
+        using (var drawingContext = contentVisual.RenderOpen())
+            drawingContext.DrawDrawing(CreateContent());
+
+        AddVisualChild(contentVisual);
+
         Loaded           += OnLoaded;
         Unloaded         += OnUnloaded;
         IsVisibleChanged += (_, _) => UpdatePlayback(this, EventArgs.Empty);
         IsEnabledChanged += (_, _) => UpdatePlayback(this, EventArgs.Empty);
     }
 
-    protected override void OnRender
-    (
-        DrawingContext drawingContext
-    ) =>
-        drawingContext.DrawRectangle(frameBrush, null, new Rect(RenderSize));
+    protected override int VisualChildrenCount => 1;
 
-    private static void OnFrameIndexChanged
+    protected override Visual GetVisualChild
     (
-        DependencyObject                   d,
-        DependencyPropertyChangedEventArgs e
+        int index
+    ) =>
+        index == 0 ?
+            contentVisual :
+            throw new ArgumentOutOfRangeException(nameof(index));
+
+    protected override void OnRenderSizeChanged
+    (
+        SizeChangedInfo sizeInfo
     )
     {
-        var animation = (LoadingAnimation)d;
-        var frame     = (int)e.NewValue % FRAME_COUNT;
-        animation.frameBrush.Viewbox = new Rect
+        base.OnRenderSizeChanged(sizeInfo);
+
+        sizeTransform.ScaleX = sizeInfo.NewSize.Width  / DESIGN_SIZE;
+        sizeTransform.ScaleY = sizeInfo.NewSize.Height / DESIGN_SIZE;
+    }
+
+    private static DoubleAnimationUsingKeyFrames CreatePulseAnimation
+    (
+        double from,
+        double to
+    )
+    {
+        var easing = new SineEase { EasingMode = EasingMode.EaseInOut };
+
+        var animation = new DoubleAnimationUsingKeyFrames
+        {
+            Duration       = TimeSpan.FromSeconds(LOOP_SECONDS),
+            RepeatBehavior = RepeatBehavior.Forever
+        };
+        animation.KeyFrames.Add(new LinearDoubleKeyFrame(from, KeyTime.FromTimeSpan(TimeSpan.Zero)));
+        animation.KeyFrames.Add(new EasingDoubleKeyFrame(to,   KeyTime.FromTimeSpan(TimeSpan.FromSeconds(LOOP_SECONDS / 2))) { EasingFunction = easing });
+        animation.KeyFrames.Add(new EasingDoubleKeyFrame(from, KeyTime.FromTimeSpan(TimeSpan.FromSeconds(LOOP_SECONDS))) { EasingFunction     = easing });
+
+        return animation;
+    }
+
+    private static Geometry CreateArcGeometry()
+    {
+        var figure = new PathFigure
+        {
+            StartPoint = PointOnRing(ARC_START_ANGLE),
+            IsClosed   = false,
+            IsFilled   = false
+        };
+        figure.Segments.Add
         (
-            (double)(frame % ATLAS_COLUMNS)                 / ATLAS_COLUMNS,
-            (double)(frame / ATLAS_COLUMNS) * ATLAS_COLUMNS / FRAME_COUNT,
-            1d                                              / ATLAS_COLUMNS,
-            (double)ATLAS_COLUMNS                           / FRAME_COUNT
+            new ArcSegment
+            (
+                PointOnRing(ARC_START_ANGLE + ARC_SWEEP_ANGLE),
+                new Size(RING_RADIUS, RING_RADIUS),
+                0,
+                ARC_SWEEP_ANGLE > 180,
+                SweepDirection.Clockwise,
+                true
+            )
         );
+
+        var geometry = new PathGeometry();
+        geometry.Figures.Add(figure);
+
+        return geometry;
+    }
+
+    private static Point PointOnRing
+    (
+        double degrees
+    )
+    {
+        var radians = degrees * Math.PI / 180;
+
+        return new Point(CENTER.X + (RING_RADIUS * Math.Sin(radians)), CENTER.Y - (RING_RADIUS * Math.Cos(radians)));
+    }
+
+    private static Pen CreatePen
+    (
+        double opacity
+    ) =>
+        Freeze
+        (
+            new Pen
+            (
+                Freeze(new SolidColorBrush(TINT) { Opacity = opacity }),
+                RING_THICKNESS
+            )
+        );
+
+    private static T Freeze<T>
+    (
+        T freezable
+    )
+        where T : Freezable
+    {
+        freezable.Freeze();
+
+        return freezable;
+    }
+
+    private DrawingGroup CreateContent()
+    {
+        var ripple = new DrawingGroup { Transform = rippleScale };
+        ripple.Children.Add(new GeometryDrawing(rippleBrush, null, RIPPLE_GEOMETRY));
+
+        var arc = new DrawingGroup { Transform = arcRotation };
+        arc.Children.Add(new GeometryDrawing(null, ARC_PEN, ARC_GEOMETRY));
+
+        var content = new DrawingGroup();
+        content.Children.Add(ripple);
+        content.Children.Add(new GeometryDrawing(null, TRACK_PEN, TRACK_GEOMETRY));
+        content.Children.Add(arc);
+
+        return content;
+    }
+
+    private static AnimationClock Apply
+    (
+        Animatable         target,
+        DependencyProperty property,
+        AnimationTimeline  animation
+    )
+    {
+        var clock = (AnimationClock)animation.CreateClock(true);
+        clock.Controller!.Begin();
+        target.ApplyAnimationClock(property, clock);
+
+        return clock;
+    }
+
+    private List<AnimationClock> StartPlayback()
+    {
+        var rotation = new DoubleAnimation(0, LOOP_DEGREES, TimeSpan.FromSeconds(LOOP_SECONDS))
+        {
+            RepeatBehavior = RepeatBehavior.Forever
+        };
+
+        return
+        [
+            Apply(arcRotation, RotateTransform.AngleProperty, rotation),
+            Apply(rippleScale, ScaleTransform.ScaleXProperty, CreatePulseAnimation(1,              RIPPLE_MAX_RADIUS / RIPPLE_RADIUS)),
+            Apply(rippleScale, ScaleTransform.ScaleYProperty, CreatePulseAnimation(1,              RIPPLE_MAX_RADIUS / RIPPLE_RADIUS)),
+            Apply(rippleBrush, Brush.OpacityProperty,         CreatePulseAnimation(RIPPLE_OPACITY, RIPPLE_MIN_OPACITY))
+        ];
     }
 
     private void OnLoaded
@@ -93,6 +230,14 @@ public sealed class LoadingAnimation : FrameworkElement
 
         hostWindow = null;
         UpdatePlayback(sender, e);
+
+        if (clocks == null)
+            return;
+
+        foreach (var clock in clocks)
+            clock.Controller!.Remove();
+
+        clocks = null;
     }
 
     private void UpdatePlayback
@@ -116,41 +261,15 @@ public sealed class LoadingAnimation : FrameworkElement
 
         if (isPlaying)
         {
-            var animation = new Int32Animation
-            (
-                0,
-                FRAME_COUNT,
-                TimeSpan.FromSeconds(LOOP_SECONDS)
-            )
-            {
-                RepeatBehavior = RepeatBehavior.Forever
-            };
-            Timeline.SetDesiredFrameRate(animation, PLAYBACK_FRAME_RATE);
-            playbackClock = (AnimationClock)animation.CreateClock(true);
-            ApplyAnimationClock(FrameIndexProperty, playbackClock);
-            playbackClock.Controller!.SeekAlignedToLastTick(position, TimeSeekOrigin.BeginTime);
+            clocks ??= StartPlayback();
+
+            foreach (var clock in clocks)
+                clock.Controller!.Resume();
         }
-        else if (playbackClock != null)
+        else if (clocks != null)
         {
-            position = playbackClock.CurrentTime ?? TimeSpan.Zero;
-            var frame = (int)GetValue(FrameIndexProperty);
-            playbackClock.Controller!.Remove();
-            ApplyAnimationClock(FrameIndexProperty, null);
-            SetCurrentValue(FrameIndexProperty, frame);
-            playbackClock = null;
+            foreach (var clock in clocks)
+                clock.Controller!.Pause();
         }
-    }
-
-    private static BitmapImage LoadAtlas()
-    {
-        var atlas = new BitmapImage();
-        atlas.BeginInit();
-        atlas.CacheOption      = BitmapCacheOption.OnLoad;
-        atlas.DecodePixelWidth = ATLAS_DECODE_WIDTH;
-        atlas.UriSource        = new Uri("pack://application:,,,/XIVLauncherCN;component/Resources/loading-spinner.png");
-        atlas.EndInit();
-        atlas.Freeze();
-
-        return atlas;
     }
 }
